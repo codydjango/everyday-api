5ffe83dabb530d0044ddaa5befcc566f64988805
Cody Django Redmond <codydjango@gmail.com>
2019-02-26 12:09:28 -0800

    add graceful shutdown

--- src/main.go
+++ src/main.go
@@ -1,12 +1,13 @@
-// https://www.alexedwards.net/blog/golang-response-snippets
-
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/rs/cors"
@@ -42,6 +43,25 @@ func main() {
 		Handler:      cors.Handler(router),
 	}
 
-	log.Printf("Running on localhost:%s", PORT)
-	log.Fatal(srv.ListenAndServe())
+	// https://github.com/gorilla/mux#graceful-shutdown
+	log.Println(fmt.Sprintf("Running on localhost:%s", PORT))
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("Shutting down...")
+	os.Exit(0)
 }
