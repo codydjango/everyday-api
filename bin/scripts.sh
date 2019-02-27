90f0a81587a10f7a5276dd04d788271a47e8b5de
Cody Django Redmond <codydjango@gmail.com>
2019-02-27 13:45:59 -0800

    better path resolution

--- bin/scripts.sh
+++ bin/scripts.sh
@@ -1,18 +1,17 @@
-BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-ROOT_DIR="$(dirname "$BIN_DIR")";
+ROOT_DIR=$(dirname $(cd $(dirname $0) && echo $(pwd)))
 SRC_DIR="$ROOT_DIR/src";
 
 buildServer () {
-    echo 'build goserver'
+    echo "build goserver"
     docker build -t goserver -f Dockerfile .
 }
 
 startServer () {
-    echo 'starting goserver'
+    echo "starting goserver"
     docker run --rm -p 3001:3001 -it --name goserver --mount source=$SRC_DIR,destination=/go/app/src,type=bind goserver
 }
 
 startProductionServer () {
-    echo 'starting production goserver'
+    echo "starting production goserver"
     docker run --rm -p 80:3001 -it --name goserver --mount source=$SRC_DIR,destination=/go/app/src,type=bind goserver
 }
