f912ce513a5d6d162a96682dce6cf39f2efe700c
Cody Django Redmond <codydjango@gmail.com>
2019-02-27 16:21:22 -0800

    docker setup with environments

--- bin/startProductionServer.sh
+++ bin/startProductionServer.sh
@@ -3,6 +3,7 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
 ROOT_DIR="$(dirname $BIN_DIR)";
 SRC_DIR="$ROOT_DIR/src";
-VIRTUAL_HOST=everyday.invisiblehands.ca
+VIRTUAL_HOST="everyday.invisiblehands.ca"
+ENVIRONMENT="production"
 
-docker run --rm -p 3001:3001 -it --name goserver -e VIRTUAL_HOST=${VIRTUAL_HOST} --mount source=${SRC_DIR},destination=/go/app/src,type=bind goserver
+docker run --rm -p 3001:3001 -it --name goserver -e VIRTUAL_HOST=${VIRTUAL_HOST} -e ENVIRONMENT=${ENVIRONMENT} goserver
--- bin/startServer.sh
+++ bin/startServer.sh
@@ -3,5 +3,6 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
 ROOT_DIR="$(dirname $BIN_DIR)";
 SRC_DIR="$ROOT_DIR/src";
+ENVIRONMENT="development"
 
-docker run --rm -p 3001:3001 -it --name goserver --mount source=${SRC_DIR},destination=/go/app/src,type=bind goserver
+docker run --rm -p 3001:3001 -it --name -e ENVIRONMENT=${ENVIRONMENT} goserver --mount source=${SRC_DIR},destination=/go/app/src,type=bind goserver
