dddfd0db57b6b92c7c6fa625038741514ac10597
Cody Django Redmond <codydjango@gmail.com>
2019-02-27 16:19:27 -0800

    update entrypoint for multiple environments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-reflex -s -r \.go$ -- go run *.go
+if [ -z "$ENVIRONMENT" ] 
+then
+    ENVIRONMENT="development"
+fi
+
+echo "entrypoint environment: ${ENVIRONMENT}"
+
+if [ "$ENVIRONMENT" = "production" ]
+then
+    reflex -s -r \.go$ -- go run *.go
+else
+    go run *.go
+fi
