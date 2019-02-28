a055b4315ad0fb2b6946f624081225ce34f8bfbd
Cody Django Redmond <codydjango@gmail.com>
2019-02-27 20:21:11 -0800

    .

--- bin/startProductionServer.sh
+++ bin/startProductionServer.sh
@@ -9,7 +9,7 @@ LETSENCRYPT_HOST="everyday.invisiblehands.ca";
 LETSENCRYPT_EMAIL="cody@invisiblehands.ca";
 
 docker run --detach \
-    --rm
+    --rm \
     --name goserver \
     --env "ENVIRONMENT=${ENVIRONMENT}" \
     --env "VIRTUAL_HOST=${VIRTUAL_HOST}" \
